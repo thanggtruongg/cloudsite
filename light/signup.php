d6a663be800a88edbf8d2adbb35476b521d8ac01
Thang Truong <30461254+thanggtruongg@users.noreply.github.com>
2017-09-23 13:26:29 +1000

    k

--- light/signup.php
+++ light/signup.php
@@ -1,5 +1,5 @@
 <?php
-	$page_title="login"; 
+	$page_title="signup"; 
 	include("includes/header.php");
 ?>
 <?php
@@ -11,15 +11,16 @@
     
     <section>
         <div class="container">
-            <h1 class="bottom-line">Login</h1>
-            <p class="text-center"><em>Login to receive coupons and explore more foods near you</em></p>
+            <h1 class="bottom-line">Create a New Account</h1>
+            <p class="text-center"><em>Signup now for our new updates and get regular offers and stuff</em></p>
             <div class="row">
                 <div class="col-md-6">
-                    <h2>Login</h2>
+                    <h2>Sign up</h2>
                     <form action = 'profile.php' method = 'GET' >
 						Email:	<input type = 'text' size='90' name =  'search' >
+						Name:	<input type = 'text' size='90' name =  'search' >
 						Password:<input type = 'password' size='90' name =  'search' >
-						<input type = 'submit' name = 'submit' value = 'Sign in' >
+						<input type = 'submit' name = 'submit' value = 'Sign Up' >
 				
 					</form>
                 </div>
